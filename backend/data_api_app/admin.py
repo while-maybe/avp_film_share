12f0a87a2998592da9f908a1cb6b2883c22b79ad
username <178353419+while-maybe@users.noreply.github.com>
2024-12-09 01:29:01 +0000

    feat: add models to admin.py

--- backend/data_api_app/admin.py
+++ backend/data_api_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Film
+
 # Register your models here.
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'date_uploaded']
+    list_filter = ['author', 'date_uploaded']
+    search_fields = ['title', 'author', 'date_uploaded', 'comment']
+    raw_id_fields = ['author']
+    ordering = ['date_uploaded']
+    show_facets = admin.ShowFacets.ALWAYS
+    
+    
