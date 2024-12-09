82721684146df6950f4217a91be980fa0b705191
username <178353419+while-maybe@users.noreply.github.com>
2024-12-09 02:08:03 +0000

    feat: add author model to admin.py

--- backend/data_api_app/admin.py
+++ backend/data_api_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Film
+from .models import Film, Author
 
 # Register your models here.
 @admin.register(Film)
@@ -12,4 +12,11 @@ class FilmAdmin(admin.ModelAdmin):
     ordering = ['date_uploaded']
     show_facets = admin.ShowFacets.ALWAYS
     
-    
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'num_uploads', 'date_joined']
+    list_filter = ['username', 'date_joined']
+    search_fields = ['username', 'email', 'num_uploads', 'date_joined']
+    # raw_id_fields = ['username']
+    ordering = ['date_joined']
+    show_facets = admin.ShowFacets.ALWAYS
