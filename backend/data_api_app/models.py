994ba202b28bc62f3c76ec412b61c5fc89530502
username <178353419+while-maybe@users.noreply.github.com>
2024-12-09 01:06:24 +0000

    feat: add Film model

--- backend/data_api_app/models.py
+++ backend/data_api_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='user_videos'
+    )
+    
+    class Meta:
+        ordering = ['-date_uploaded']
+    
+    def __str__(self):
+        return self.title
+    
+    
