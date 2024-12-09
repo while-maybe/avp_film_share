e3284bb5206d0ca8b1b533047965443ff7449c85
username <178353419+while-maybe@users.noreply.github.com>
2024-12-09 21:25:05 +0000

    fix: add '/' to url termination (duh!)

--- backend/data_api_app/urls.py
+++ backend/data_api_app/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.get_video),
-    path('all', views.all_videos),
-    path('read', views.get_video),
-    path('create', views.add_video),
-    path('delete', views.del_video),
-    path('update', views.edit_video),
+    path('all/', views.all_videos),
+    path('read/', views.get_video),
+    path('create/', views.add_video),
+    path('delete/', views.del_video),
+    path('update/', views.edit_video),
 ]
-
