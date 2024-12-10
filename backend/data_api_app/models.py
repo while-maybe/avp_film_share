976d9cf748411d989687490ee60a2d3eac5882c5
username <178353419+while-maybe@users.noreply.github.com>
2024-12-10 02:28:07 +0000

    refactor: set unique for email, title, location, slug

--- backend/data_api_app/models.py
+++ backend/data_api_app/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Video(models.Model):
     video_id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=150)
-    location = models.URLField(default="")
+    title = models.CharField(max_length=150, unique=True)
+    location = models.URLField(default="", unique=True)
     author = models.ForeignKey(
         'Author',
         on_delete=models.CASCADE,
         related_name='user_videos'
     )
     comment = models.TextField(max_length=1000, blank=True)
-    slug = models.SlugField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
     date_uploaded = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
     
@@ -37,6 +37,7 @@ class Author(AbstractUser):
     author_id = models.AutoField(primary_key=True)
     about = models.TextField(max_length=1000, blank=True)
     num_uploads = models.IntegerField(default=0)
+    email = models.EmailField(unique=True)
     
     def __str__(self):
         return self.username
