e0722210297dfd1411f2fa74f37cc10795581503
username <178353419+while-maybe@users.noreply.github.com>
2024-12-10 01:25:12 +0000

    feat : add swagger

--- backend/auth_api_app/views.py
+++ backend/auth_api_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework.status import *
 
 # Create your views here.
+def signup(request):
+    
+    return Response({"message": "signup endpoint"})
+
+
+def login(request):
+    
+    return Response({"message": "login endpoint"})
+
+
+def logout(request):
+    
+    return Response({"message": "logout endpoint"})
+
+
+def test_view(request):
+    
+    return Response({"message": "test_view endpoint"})
--- backend/data_api_app/urls.py
+++ backend/data_api_app/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_video),
     path('all/', views.all_videos),
     path('read/', views.get_video),
     path('create/', views.add_video),
