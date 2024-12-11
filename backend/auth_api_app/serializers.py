d5978389cc127bcf7bdd3c92743a909046e4de53
username <178353419+while-maybe@users.noreply.github.com>
2024-12-11 20:01:56 +0000

    feat: add write-only to pwd in auth serializer

--- backend/auth_api_app/serializers.py
+++ backend/auth_api_app/serializers.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 
 from rest_framework.authtoken.models import Token
 # import from the Users model in the data_api_app project
 from data_api_app.models import Author
 
-class AuthorSerializer(ModelSerializer):
+class AuthorSerializer(ModelSerializer):    
     class Meta:
         model = Author
-        fields = ['author_id', 'username', 'email', 'password']
+        fields = ['username', 'email', 'password']
+        # extra_kwargs customizes behavior of model fields
+        # we don't send plain text password back int the response!
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
         
     # override default save to include token
     def save(self, **kwargs):
@@ -18,6 +24,7 @@ class AuthorSerializer(ModelSerializer):
             password = self.validated_data["password"],
         )
         
-        new_author.save()
+        token = Token.objects.create(user=new_author)
+        # new_author.save()
+        return new_author, token
         
-        new_token = Token.objects.create(user=new_author)
