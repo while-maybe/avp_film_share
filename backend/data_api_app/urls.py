3479f74c3c48deb8493e48e62a871b8e6976e707
username <178353419+while-maybe@users.noreply.github.com>
2024-12-11 00:06:05 +0000

    feat: add urls.py app_name & namespace config
    Add app_name to urls.py on both apps data_api, auth_api
    Add matching namespace on project urls.py for data_api, auth_api

--- backend/data_api_app/urls.py
+++ backend/data_api_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'data_api_app'
+
 urlpatterns = [
     path('all/', views.all_videos),
     path('read/', views.get_video),
     path('create/', views.add_video),
     path('delete/', views.del_video),
     path('update/', views.edit_video),
+    
+    # class based
+    path('list/', views.VideoListView.as_view(), name='video_list')
 ]
