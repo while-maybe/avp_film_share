50834c4c224626cfffaba01f31af3510edc6bd6b
username <178353419+while-maybe@users.noreply.github.com>
2024-12-12 02:24:49 +0000

    feat: add 'author_username' to Video serializer

--- backend/data_api_app/serializers.py
+++ backend/data_api_app/serializers.py
@@ -1,10 +1,13 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Video
 
 class VideoSerializer(ModelSerializer):
+    author_username = SerializerMethodField()
+    
+    def get_author_username(self, obj):
+        return obj.author_id.username
+    
     class Meta:
         model = Video
-        fields = ['video_id', 'title', 'location', 'comment', 'slug', 'date_released', 'likes']
+        fields = ['video_id', 'title', 'location', 'description', 'slug', 'date_released', 'likes', 'author_username']
         read_only_fields = ['video_id', 'author_id', 'date_uploaded', 'likes', 'slug']
-
-    
