fc2d751864cb28f4c852cc164c30ea65fd43d871
username <178353419+while-maybe@users.noreply.github.com>
2024-12-16 19:40:09 +0000

    feat: add axios, api comm interceptor

---
+++ frontend/src/api/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("authToken");
+    console.log(token);
+    if (token) {
+      // 'Token' instead of 'Bearer'
+      config.headers.Authorization = `Token ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
