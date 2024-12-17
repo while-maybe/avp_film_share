c2342acb6a800df2c3055391738ef042b6b062b6
username <178353419+while-maybe@users.noreply.github.com>
2024-12-17 03:39:19 +0000

    style: make space for thumbnail

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -42,13 +42,15 @@ const VideoCard = ({
   };
 
   return (
-    <Card className="mt-6 w-96 bg-blue-gray-100">
+    <Card className="mt-6 w-full bg-blue-gray-100 flex">
       <Dialog open={open} handler={handleOpen}>
         <DialogHeader>Delete this Video?</DialogHeader>
+
         <DialogBody>
           This action will permanently delete this video. Are you sure you want
           to continue?
         </DialogBody>
+
         <DialogFooter>
           <Button
             variant="text"
@@ -57,46 +59,57 @@ const VideoCard = ({
             className="mr-1">
             <span>Cancel</span>
           </Button>
+
           <Button variant="gradient" color="red" onClick={handleDelete}>
             <span>Confirm</span>
           </Button>
         </DialogFooter>
       </Dialog>
 
-      <CardBody>
-        <div className="flex justify-between items-center">
-          <Typography variant="h5" color="blue-gray" className="mb-2">
-            {title}
-          </Typography>
-
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            size="sm"
-            onClick={handleOpen}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              x="0px"
-              y="0px"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24">
-              <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
-            </svg>
-          </IconButton>
+      <CardBody className="flex">
+        <div className="flex items-center p-4">
+          <p>this is just for the video thumbnail</p>
         </div>
 
-        <Typography className="min-h-12">
-          {truncateText(description, 80)}
-        </Typography>
-        <Typography color="blue-gray" variant="small">
-          {formatDistanceToNow(date_uploaded, { addSuffix: true })}
-        </Typography>
+        <div className="w-full">
+          <div className="flex justify-between items-center">
+            <Typography variant="h5" color="blue-gray" className="mb-2">
+              {title}
+            </Typography>
+
+            <IconButton
+              variant="text"
+              color="blue-gray"
+              size="sm"
+              onClick={handleOpen}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24">
+                <path d="M 10 2 L 9 3 L 3 3 L 3 5 L 4.109375 5 L 5.8925781 20.255859 L 5.8925781 20.263672 C 6.023602 21.250335 6.8803207 22 7.875 22 L 16.123047 22 C 17.117726 22 17.974445 21.250322 18.105469 20.263672 L 18.107422 20.255859 L 19.890625 5 L 21 5 L 21 3 L 15 3 L 14 2 L 10 2 z M 6.125 5 L 17.875 5 L 16.123047 20 L 7.875 20 L 6.125 5 z"></path>
+              </svg>
+            </IconButton>
+          </div>
+
+          <Typography className="min-h-12">
+            {truncateText(description, 80)}
+          </Typography>
+          <Typography color="blue-gray" variant="small">
+            {formatDistanceToNow(date_uploaded, { addSuffix: true })}
+          </Typography>
+
+          <Button className="mt-2" onClick={() => onReadMore(video_id)}>
+            Read More
+          </Button>
+        </div>
       </CardBody>
 
-      <CardFooter className="pt-0">
+      {/* <CardFooter className="pt-0">
         <Button onClick={() => onReadMore(video_id)}>Read More</Button>
-      </CardFooter>
+      </CardFooter> */}
     </Card>
   );
 };
